7b8dc54944e961bcc72b8e2757427bce4bb2d858
dariushKingking000 <kingkingplmkoyeb@gmail.com>
2026-04-23 18:44:28 +0330

    Create playwright.config.js

---
+++ playwright.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testDir: './tests',
+  projects: [
+    { name: 'chromium', use: { browserName: 'chromium' } },
+    { name: 'firefox', use: { browserName: 'firefox' } },
+    { name: 'webkit', use: { browserName: 'webkit' } }
+  ]
+};
