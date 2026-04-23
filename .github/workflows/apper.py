80013bef84575107d9cad64829bcd748de07a949
dariushKingking000 <kingkingplmkoyeb@gmail.com>
2026-04-23 18:35:26 +0330

    Delete .github/workflows/apper.py

--- .github/workflows/apper.py
+++
@@ -1,9 +0,0 @@
-with open("google_results.txt", "w") as f:
-    f.write("نتایج سرچ Python Tutorial:\n\n")
-    f.write("1. W3Schools - https://w3schools.com/python\n")
-    f.write("2. GeeksforGeeks - https://geeksforgeeks.org/python\n")
-    f.write("3. Python.org - https://python.org\n")
-    f.write("4. RealPython - https://realpython.com\n")
-    f.write("5. freeCodeCamp - https://freecodecamp.org/python\n")
-
-print("✅ فایل OK!")
